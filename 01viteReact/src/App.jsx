a35471e7a63d05d5421aea9fa526b907fd1be8f5
sachin <sachin_yadav@arundataprocessing.co.in>
2026-01-13 11:05:55 +0530

    create vite react file

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,16 @@
+
+import One from "./one"
+
+
+function App() {
+
+  return (
+    <>
+    <One/>
+    <p>para</p>
+    <li>list 1</li>
+    </>
+  )
+}
+
+export default App
---
+++ 01viteReact/src/one.jsx
@@ -0,0 +1,11 @@
+
+
+
+
+function one() {
+    return(
+        <h3>Sachin</h3>
+    )
+}
+
+export default one
