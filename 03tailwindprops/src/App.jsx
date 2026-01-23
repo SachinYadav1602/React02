55a7d8bc0c3131eec69399b72f2343b8968d1485
sachin <sachin_yadav@arundataprocessing.co.in>
2026-01-23 08:52:30 +0530

    tailwind project modified

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,28 +8,35 @@ function App() {
 
   return (
     <>
-      <div className='bg-red-500'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+ <div className="max-w-xs rounded-md shadow-md bg-black text-gray-100">
+      <img
+        src="https://picsum.photos/301"
+        alt=""
+        className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500"
+      />
+      <div className="flex flex-col justify-between p-6 space-y-8">
+        <div className="space-y-2">
+          <h2 className="text-3xl font-semibold tracking-wide">Lorem</h2>
+          <p className="text-gray-400">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
+            tempora ipsum soluta amet corporis accusantium aliquid consectetur
+            eaque!
+          </p>
+        </div>
+        <button
+          type="button"
+          className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-gray-800 text-gray-200"
+        >
+          Read more
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+
     </>
   )
 }
 
+
+
 export default App
+
